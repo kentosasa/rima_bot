44094c909f851c705c70ab45ae5cf5f174707e08
kento sasamoto <knote0721@gmail.com>
2017-01-14 15:00:12 +0900

    出席ユーザー一覧, 欠席ユーザー一覧を取得するメソッドの生成

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -12,4 +12,14 @@
 class Candidate < ApplicationRecord
   has_many :candidate_user_relations
   has_many :users, :through => :candidate_user_relations
+
+  def attend_users
+    ids = self.candidate_user_relations.where(attend: true).pluck(:user_id)
+    users = User.where(id: ids)
+  end
+
+  def absent_users
+    ids = self.candidate_user_relations.where(attend: false).pluck(:user_id)
+    users = User.where(id: ids)
+  end
 end
