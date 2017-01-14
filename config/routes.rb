3af4e83984c859ce60b15ac4d1a8cb178ff53192
pokohide <hyde31415@gmail.com>
2017-01-14 19:54:32 +0900

    remindsコントーローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'reminds/index'
+
+  get 'reminds/new'
+
+  get 'reminds/create'
+
+  get 'reminds/update'
+
+  get 'reminds/edit'
+
+  get 'reminds/destroy'
+
   post '/', to: 'webhook#callback'
   post '/callback', to: 'webhook#callback'
 end
