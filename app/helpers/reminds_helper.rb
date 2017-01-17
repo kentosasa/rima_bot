736d05a3f182a6e27972f10fc90f96c6662dcc8d
pokohide <hyde31415@gmail.com>
2017-01-17 20:05:48 +0900

    グーグルカレンダーへの登録ボタン追加

--- app/helpers/reminds_helper.rb
+++ app/helpers/reminds_helper.rb
@@ -1,2 +1,15 @@
 module RemindsHelper
+
+  def google_cal_link(remind)
+    url = 'http://www.google.com/calendar/event?'
+    url += 'action=TEMPLATE'
+    url += '&text=' + remind.name
+    url += '&details=' + remind.body if remind.body.present?
+    url += '&date=' + remind.datetime.strftime("%Y%m%dT%H%m%SZ")
+    url += '&location=' + remind.address if remind.address.present?
+    url += '&trp=false'
+    url += '&sprop=リマさん'
+    #url += '&sprop=' + # ここのリマインドの詳細リンク
+    url
+  end
 end
