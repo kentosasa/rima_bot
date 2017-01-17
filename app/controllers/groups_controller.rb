fdee1d8e208fbe8990655f05b30f4018e98f3c76
pokohide <hyde31415@gmail.com>
2017-01-17 19:16:34 +0900

    ヘッダー追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,11 +9,21 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      flash[:success] = '設定を更新しました。'
+      redirect_to group_path(@group)
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def set_group
     @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
