fa139585b17a6068444fadfb9596495fd8db8d67
pokohide <hyde31415@gmail.com>
2017-01-17 18:34:52 +0900

    設定画面のルーティング設定

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,14 @@
+class GroupsController < ApplicationController
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def reminds
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   post '/callback', to: 'webhook#callback'
 
-  resources :reminds do
+  resources :reminds, except: [:index] do
     member do
       get :activate
+    end
+  end
+
+  resources :groups, only: [:show, :edit, :update] do
+    member do
+      get :reminds
+      get 'reminds/new', to: 'reminds#new'
     end
   end
 end
