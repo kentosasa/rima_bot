d8e24f343dcc96b3d83e0fe864341eed49cfdc0a
pokohide <hyde31415@gmail.com>
2017-02-06 01:55:20 +0900

    meta-tagをちゃんと追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,6 +19,37 @@ module ApplicationHelper
       viewport: 'width=device-width, initial-scale=1.0'
     }
   end
+
+  def top_meta_tags
+    {
+      site: 'LINEで使える簡単リマインドBOT リマさん',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1.0',
+      keywords: 'LINE,BOT,リマインド,通知,簡単,日程調整',
+      description: 'LINEで友達と、家族とサークルで使える簡単リマインドBOT リマさんです。BOTを意識することなく、通知や日程調整のサポートをしてくれます。',
+      open_graph: {
+        title: 'LINEで使える簡単リマインドBOT リマさん',
+        url: request.original_url
+      },
+      og: {
+        title: 'LINEで使える簡単リマインドBOT リマさん',
+        type: 'website',
+        url: request.original_url,
+        image: asset_path('logo.png'),
+        site_name: 'リマさん',
+        description: 'LINEで友達と、家族とサークルで使える簡単リマインドBOT リマさんです。BOTを意識することなく、通知や日程調整のサポートをしてくれます。',
+        localer: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary',
+        #site: ''
+        title: 'LINEで使える簡単リマインドBOT リマさん',
+        description: 'LINEで友達と、家族とサークルで使える簡単リマインドBOT リマさんです。BOTを意識することなく、通知や日程調整のサポートをしてくれます。',
+        image: asset_path('logo.png')
+      }
+    }
+  end
+
   def original_url
     ENV['WEBHOOK_URL'] + request.fullpath
   end
